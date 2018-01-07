e857b47eee459fc239b84542f2d1743042dc6795
Jeremy R DeYoung <Jeremy@NextStepGuru.com>
2018-01-07 13:34:10 -0600

    More updates

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,22 @@
         .navbar-brand
           router-link.navbar-item(to="/")
             span VueStacks.com
+          button.button.navbar-burger(@click='toggleMenu')
+            span
+            span
+            span
 
-        .navbar-start
-          router-link.navbar-item(to="/data-table") Data-Table
+        .navbar-menu
+          .navbar-start
+            .navbar-item.has-dropdown.is-hoverable
+              a.navbar-link Components
+              .navbar-dropdown.is-boxed
+                router-link.navbar-item(to="/data-table") Data-Table
 
-        .navbar-end
-          a.navbar-item(href="https://github.com/blackfisk-tech/vuestacks.com", title="VueStacks.com GitHub")
-            i.fa.fa-github
-
-        button.button.navbar-burger
-          span
-          span
-          span
+          .navbar-end
+            a.navbar-item(href="https://github.com/blackfisk-tech/vuestacks.com", title="VueStacks.com GitHub")
+              i.fab.fa-github
+              span &nbsp; View on Github
     main.is-fluid.Site-content
       router-view
     footer.footer
@@ -25,8 +29,15 @@
           span.logotype-font-family.copyright &copy; VueStacks.com {{ new Date().getFullYear() }}</template>
 
 <script>
+import '@fortawesome/fontawesome'
+import '@fortawesome/fontawesome-free-brands'
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    toggleMenu () {
+      document.getElementsByClassName('navbar-menu')[0].classList.toggle('is-active')
+    }
+  }
 }
 </script>
 
--- src/components/DataTableView.vue
+++ src/components/DataTableView.vue
@@ -1,186 +1,188 @@
 <template lang="pug">
   .container
-    h1.is-size-1 Data Tables
+    h1.is-size-1 Data Table
 
     h2.is-size-3 Code Snippet
-    h4.title.is-5 A simple vue.js example component
-    pre.html
-      |&lt;template&gt;
-      |   &lt;div class="container"&gt;
-      |     &lt;data-table :payload='data' /&gt;
-      |   &lt;/div&gt;
-      |&lt;/template&gt;
-      |
-      |&lt;script&gt;
-      |import DataTable from 'vstx-data-table'
-      |
-      |export default {
-      |  name: 'data-table-example',
-      |  components: {
-      |    'data-table': DataTable
-      |  },
-      |  data () {
-      |    return {
-      |      data: []
-      |    }
-      |  }
-      |}
-      |&lt;/script&gt;
-
+    section.section
+      h4.title.is-5 A simple vue.js example component
+      pre.html
+        |&lt;template&gt;
+        |   &lt;div class="container"&gt;
+        |     &lt;data-table :payload='data' /&gt;
+        |   &lt;/div&gt;
+        |&lt;/template&gt;
+        |
+        |&lt;script&gt;
+        |import DataTable from 'vstx-data-table'
+        |
+        |export default {
+        |  name: 'data-table-example',
+        |  components: {
+        |    'data-table': DataTable
+        |  },
+        |  data () {
+        |    return {
+        |      data: []
+        |    }
+        |  }
+        |}
+        |&lt;/script&gt;
 
     h2.is-size-3 Table Configuration Payload
-    .columns
-      .column
-        h4.title.is-5 Description
-        aside.menu(v-for='i in configurationOutput()')
-          ul.menu-list
-            p.menu-label {{ i.key }}
-            li
-              ul
-                template(v-for='(k, o) in i.data')
-                  li(v-if="typeof k === 'object'")
-                    span.menu-label {{ o }}
-                    ul
-                      template(v-for='(a, b) in k')
-                        li
-                          span.menu-label {{ b }}
-                          span  : {{ a }}
-                  li(v-else="")
-                    span.menu-label {{ o }}
-                    span  : {{ k }}
-      .column
-        h4.title.is-5 Configuration Payload Data-Type
-        pre.javascript
-          |{
-          |  "collapsePages": Boolean,
-          |  "isRanked": Boolean,
-          |  "table": {
-          |    "bordered": Boolean,
-          |    "striped": Boolean,
-          |    "cellbordered": Boolean,
-          |    "overflow": Boolean,
-          |    "hoverable": Boolean,
-          |    "fullwidth": Boolean,
-          |    "filename": String,
-          |    "isSelectable": Boolean
-          |  },
-          |  "settings": {
-          |    "isVisible": Boolean,
-          |    "isAllowed": Boolean,
-          |    "offset": Number
-          |  },
-          |  "sortIndicator": {
-          |    "isVisible": Boolean,
-          |    "isAllowed": Boolean
-          |  },
-          |  "filter": {
-          |    "isVisible": Boolean,
-          |    "isAllowed": Boolean,
-          |    "isEvent": Boolean
-          |  },
-          |  "columns": {
-          |    "isVisible": Boolean,
-          |    "isAllowed": Boolean
-          |  },
-          |  "pagination": {
-          |    "rowsPerPage": Number,
-          |    "isAllowed": Boolean,
-          |    "isVisible": Boolean
-          |  },
-          |  "orderBy": {
-          |    "isVisible": Boolean,
-          |    "isAllowed": Boolean
-          |  },
-          |  "totals": {
-          |    "isVisible": {
-          |      "all": Boolean,
-          |      "page": Boolean,
-          |      "count": Boolean
-          |    },
-          |    "isAllowed": Boolean
-          |  }
-          |}
+    section.section
+      .columns
+        .column
+          h4.title.is-5 Description
+          aside.menu(v-for='i in configurationOutput()')
+            ul.menu-list
+              p.menu-label {{ i.key }}
+              li
+                ul
+                  template(v-for='(k, o) in i.data')
+                    li(v-if="typeof k === 'object'")
+                      span.menu-label {{ o }}
+                      ul
+                        template(v-for='(a, b) in k')
+                          li
+                            span.menu-label {{ b }}
+                            span  : {{ a }}
+                    li(v-else="")
+                      span.menu-label {{ o }}
+                      span  : {{ k }}
+        .column
+          h4.title.is-5 Configuration Payload Data-Type
+          pre.javascript
+            |{
+            |  "collapsePages": Boolean,
+            |  "isRanked": Boolean,
+            |  "table": {
+            |    "bordered": Boolean,
+            |    "striped": Boolean,
+            |    "cellbordered": Boolean,
+            |    "overflow": Boolean,
+            |    "hoverable": Boolean,
+            |    "fullwidth": Boolean,
+            |    "filename": String,
+            |    "isSelectable": Boolean
+            |  },
+            |  "settings": {
+            |    "isVisible": Boolean,
+            |    "isAllowed": Boolean,
+            |    "offset": Number
+            |  },
+            |  "sortIndicator": {
+            |    "isVisible": Boolean,
+            |    "isAllowed": Boolean
+            |  },
+            |  "filter": {
+            |    "isVisible": Boolean,
+            |    "isAllowed": Boolean,
+            |    "isEvent": Boolean
+            |  },
+            |  "columns": {
+            |    "isVisible": Boolean,
+            |    "isAllowed": Boolean
+            |  },
+            |  "pagination": {
+            |    "rowsPerPage": Number,
+            |    "isAllowed": Boolean,
+            |    "isVisible": Boolean
+            |  },
+            |  "orderBy": {
+            |    "isVisible": Boolean,
+            |    "isAllowed": Boolean
+            |  },
+            |  "totals": {
+            |    "isVisible": {
+            |      "all": Boolean,
+            |      "page": Boolean,
+            |      "count": Boolean
+            |    },
+            |    "isAllowed": Boolean
+            |  }
+            |}
 
     h2.is-size-3 Table Columns Payload
-    .columns
-      .column
-        h4.title.is-5 Description
-        aside.menu
-          ul.menu-list
-            li
-              span.menu-label Array of Columns
-              ul
-                li
-                  p.menu-label Column
-                  ul
-                    li
-                      span.menu-label name
-                      span  : Column Title
-                    li
-                      span.menu-label format
-                      span  : Format
-                      p
-                        span.menu-label &nbsp; &nbsp; enum :
-                        em  (formatString, formatPercent, formatMoney, formatNumber)
-                    li
-                      span.menu-label field
-                      span  : Field/Key Name
-                    li
-                      span.menu-label align
-                      span  : Alignment
-                      p
-                        span.menu-label &nbsp; &nbsp; enum :
-                        em  (right, left, center)
-                    li
-                      span.menu-label position
-                      span  : Column Display Position
-                    li
-                      span.menu-label isVisible
-                      span  : Show or Hide column
-                    li
-                      span.menu-label editing
-                      ul
-                        li
-                          span.menu-label edit
-                          span  : Is Editable
-                        li
-                          span.menu-label isAllowed
-                          span  : Allow editing
-                    li
-                      span.menu-label sort
-                      ul
-                        li
-                          span.menu-label isSortable
-                          span  : Allow sorting
-                        li
-                          span.menu-label direction
-                          span  : Sort Direction
-                          p
-                            span.menu-label &nbsp; &nbsp; enum :
-                            em  (asc, desc)
-                        li
-                          span.menu-label order
-                          span  : Sort Order
-      .column
-        h4.title.is-5 Column Payload Data-Type
-        pre.javascript
-          |{
-          |   "name": String,
-          |   "format": String,
-          |   "field": String,
-          |   "align": String,
-          |   "position": Number,
-          |   "isVisible": Boolean,
-          |   "editing": {
-          |     "edit": Boolean,
-          |     "isAllowed": Boolean
-          |   },
-          |   "sort": {
-          |     "isSortable": Boolean,
-          |     "direction": String,
-          |     "order": Number
-          |   }
-          |}
+    section.section
+      .columns
+        .column
+          h4.title.is-5 Description
+          aside.menu
+            ul.menu-list
+              li
+                span.menu-label Array of Columns
+                ul
+                  li
+                    p.menu-label Column
+                    ul
+                      li
+                        span.menu-label name
+                        span  : Column Title
+                      li
+                        span.menu-label format
+                        span  : Format
+                        p
+                          span.menu-label &nbsp; &nbsp; enum :
+                          em  (formatString, formatPercent, formatMoney, formatNumber)
+                      li
+                        span.menu-label field
+                        span  : Field/Key Name
+                      li
+                        span.menu-label align
+                        span  : Alignment
+                        p
+                          span.menu-label &nbsp; &nbsp; enum :
+                          em  (right, left, center)
+                      li
+                        span.menu-label position
+                        span  : Column Display Position
+                      li
+                        span.menu-label isVisible
+                        span  : Show or Hide column
+                      li
+                        span.menu-label editing
+                        ul
+                          li
+                            span.menu-label edit
+                            span  : Is Editable
+                          li
+                            span.menu-label isAllowed
+                            span  : Allow editing
+                      li
+                        span.menu-label sort
+                        ul
+                          li
+                            span.menu-label isSortable
+                            span  : Allow sorting
+                          li
+                            span.menu-label direction
+                            span  : Sort Direction
+                            p
+                              span.menu-label &nbsp; &nbsp; enum :
+                              em  (asc, desc)
+                          li
+                            span.menu-label order
+                            span  : Sort Order
+        .column
+          h4.title.is-5 Column Payload Data-Type
+          pre.javascript
+            |{
+            |   "name": String,
+            |   "format": String,
+            |   "field": String,
+            |   "align": String,
+            |   "position": Number,
+            |   "isVisible": Boolean,
+            |   "editing": {
+            |     "edit": Boolean,
+            |     "isAllowed": Boolean
+            |   },
+            |   "sort": {
+            |     "isSortable": Boolean,
+            |     "direction": String,
+            |     "order": Number
+            |   }
+            |}
 
     h2.is-size-3 Examples
     ul
